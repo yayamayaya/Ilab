83c746df969357af695e498e2e1871bdeb729e83
yayamayaya <semenov.aleksandr@phystech.edu>
2024-03-08 10:53:25 +0300

    создан енам ошибок

--- common/log.h
+++ common/log.h
@@ -3,3 +3,9 @@
 
 void fileLog(FILE *logFileName, const char *format, ...) __attribute__ ((format(printf, 2, 3)));
 FILE *openLogFile(const int argNum, const char **argv, const char *StandardFileName, const char *format, ...);
+
+enum LOG_ERRORS
+{
+    LOG_FILE_OPEN_ERR   = 1010,
+
+};
