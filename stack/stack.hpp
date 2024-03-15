b7d55bf4d4dfbe7963ea985134ab1df6f3755ef1
yayamayaya <semenov.aleksandr@phystech.edu>
2024-03-16 02:12:01 +0300

    Все функции переделаны под шаблоны, упрощена их общая структура

--- stack/stack.hpp
+++ stack/stack.hpp
@@ -4,123 +4,116 @@
 #include "../common/log.h"
 #include "stack.h"
 
-
 #ifndef STACK
 #define STACK
 
 #ifdef SECURE
-#define STACK_SIZE capacity * sizeof(T) + 2 * sizeof(canary) + sizeof(hash_t), 1
+#define STACK_SIZE                      capacity * sizeof(T) + 2 * sizeof(canary_t) + sizeof(hash_t)
+#define STACK_DATA_POSITION             sizeof(canary_t) + sizeof(hash_t)
+#define HASH_REFRESH()                    *(getHashNumberPtr()) = hashFunc()
 #else
-#define STACK_SIZE capacity * sizeof(T)
+#define STACK_DATA_POSITION             0
+#define STACK_SIZE                      capacity * sizeof(T)
+#define HASH_REFRESH()                  do {} while (0)
 #endif
 
 #ifdef DEBUG
-#define LOG(...) fileLog(this->logFile, __VA_ARGS__) 
+#define STACK_LOG(...) fileLog(this->logFile, __VA_ARGS__) 
 #define LOG_FILE_CLOSE()               \
     fclose(logFile);        \
     logFile = NULL
+#define DUMP_TO_LOGFILE()   if (!option) fileName = logFile
 #else
-#define LOG(...)
+#define DUMP_TO_LOGFILE()   do {} while (0)
+#define STACK_LOG(...)
 #define LOG_FILE_CLOSE()
 #endif
 
 const char *msgNoArgs = "%-25s| %-20s| %-20s|\n";
 const char *msgOneArg = "%-25s| %-20d| %-20s|\n";
 
-//const dataType poison = 0xDD;
-const canary Lcanary = 0xDEDDEAD;
-const canary Rcanary = 0xFFABEBA;
+//const stackData_t poison = 0xDD;
+const canary_t Lcanary = 0xDEDDEAD;
+const canary_t Rcanary = 0xFFABEBA;
+
+//#ifdef SECURE
 
 template <typename T>
-int stack<T>::stackCtor(const int capacity, const char* logFileName)
+canary_t *stack<T>:: getLeftCanaryPtr()
 {
-    //assert(this != NULL);
-    assert(capacity != 0);
+    return (canary_t *)data;
+}
 
-#ifdef DEBUG            //Открываем логфайл
+template <typename T>
+hash_t *stack<T>:: getHashNumberPtr()
+{
+    return (hash_t *)((char *)data + sizeof(canary_t));
+}
+
+template <typename T>
+T *stack<T>:: getDataPtr()
+{
+    return (T *)((char *)data + STACK_DATA_POSITION);
+}
+
+template <typename T>
+canary_t *stack<T>:: getRightCanaryPtr()
+{
+    return (canary_t *)((char *)data + sizeof(canary_t) + sizeof(hash_t) + capacity * sizeof(T));
+}
+
+template <typename T>
+int stack<T>::stackCtor(const int stackCapacity, const char* logFileName)
+{
+    assert(stackCapacity != 0);
+
+#ifdef DEBUG
     assert(logFileName != NULL);
     this->logFile = fopen(logFileName, "w");
-    if (this->logFile == NULL)  //Проверка на нулевой указатель
+    if (this->logFile == NULL)
     {
         printf("[error]>>Can't open stack log.\n");
-        return LOG_FILE_OPEN_ERR;
-        //#undef DEBUG        
+        return LOG_FILE_OPEN_ERR;     
     }
     fileLog(this->logFile, "%-25s| %-20s| %-20s|\n\n", "INSTRUCT. NAME", "ARGUMENTS", "ERROR");
 #endif
 
-/*#ifdef SECURE
-    this->leftCanaryPtr = (canary *)calloc(capacity * sizeof(T) + 2 * sizeof(canary) + sizeof(hash_t), 1);       
-    if (this->leftCanaryPtr == NULL)        //Аллоцируем память под канареек, хеш и данные, присваиваем адреса памяти канарейкам и данным
-    {
-        LOG(msgNoArgs, "MEM_ALC_ERR", "", "[error]");
-#ifdef DEBUG
-        fclose(this->logFile);
-#endif
-        return MEM_ALC_ERR;
-    }
-
-    this->hash = (hash_t *)(this->leftCanaryPtr + 1);
-    this->data_ = (T *)(this->hash + 1);
-    this->rightCanaryPtr = (canary *)(this->data_ + capacity);
-
-    *(this->leftCanaryPtr) = Lcanary;   //Присваиваем канарейкам значения
-    *(this->rightCanaryPtr) = Rcanary; 
-#else
-    this->data_ = (T *)calloc(capacity, sizeof(T));
-    if(this->data_ == NULL)
-    {
-        LOG(msgNoArgs, "MEM_ALC_ERR", "", "[error]");
-#ifdef DEBUG
-        fclose(this->logFile);
-#endif
-        return MEM_ALC_ERR;
-    }
-#endif*/
-
-    data = (T *)calloc(STACK_SIZE);
+    capacity = stackCapacity;
+    data = (T *)calloc(STACK_SIZE, 1);
+    //fprintf(stderr, "Stack size: %d; Stack pointer: %p\n", STACK_SIZE, data);
     if (data == NULL)
     {
-        LOG(msgNoArgs, "MEM_ALC_ERR", "", "[error]");
+        STACK_LOG(msgNoArgs, "MEM_ALC_ERR", "", "[error]");
         LOG_FILE_CLOSE();
         return MEM_ALC_ERR;
     }
     
+#ifdef SECURE
+    *getLeftCanaryPtr() = Lcanary;
+    *getHashNumberPtr() = hashFunc();
+    *getRightCanaryPtr() = Rcanary;
+#endif
 
-    this->size = 0;
-    this->capacity = capacity;
+    size = 0;
+    //capacity = stackCapacity;
+    poisonFunc();   
 
-    if (poisonFunc())
-        return PSN_ERR;        
-
-    *(this->hash) = hashFunc();   
-
-    LOG(msgNoArgs, "STK_CONSTED", "", ""); 
+    STACK_LOG(msgNoArgs, "STK_CONSTED", "", ""); 
     return stackVerificator();
 }
 
 template <typename T>
 int stack<T>::stackDtor()
 {
-    //assert(this != NULL);
-
     int errorNum = stackVerificator();
     
-    this->capacity = 0;
-    this->size = 0;
+    capacity = 0;
+    size = 0;
 
-#ifdef SECURE
-    free(this->leftCanaryPtr);
-        
-    this->leftCanaryPtr = NULL;        //Высвобождаем память, занулляем указатели
-    this->hash = NULL;
-    this->rightCanaryPtr = NULL;
-#else
-    free(this->data_);
-#endif
+    free(data);
 
-    this->data_ = NULL;
-    LOG(msgNoArgs, "STK_DESTTED", "", "");
+    data = NULL;
+    STACK_LOG(msgNoArgs, "STK_DESTTED", "", "");
     LOG_FILE_CLOSE();
 
     return 0;
@@ -129,23 +122,20 @@ int stack<T>::stackDtor()
 template <typename T>
 int stack<T>::stackPush(const T num)
 {
-    assert(this != NULL);
-
     int errorNum = stackVerificator();
     if (errorNum)
         return errorNum;
 
-    if (this->size == this->capacity)           //Проверка на переполнение массива
+    if (size == capacity)           //Проверка на переполнение массива
         if(stk_realloc(UP))
             return MEM_RLC_ERR;
     
-    this->data_[this->size] = num;
-    LOG("%-25s| %-20lld| %-20s|\n", "STK_PUSH", this->data_[this->size], "");
-    this->size++;
+    *(getDataPtr() + size) = num;
+    //data[this->size] = num;
+    STACK_LOG("%-25s| %-20lld| %-20s|\n", "STK_PUSH", *(getDataPtr() + size), "");
+    size++;
 
-#ifdef SECURE
-    *(this->hash) = hashFunc();
-#endif
+    HASH_REFRESH();
     
     return stackVerificator();
 }
@@ -153,64 +143,58 @@ int stack<T>::stackPush(const T num)
 template <typename T>
 int stack<T>::stackPop(T *num)
 {    
-    assert(this != NULL);
-
     int errorNum = stackVerificator();
     if (errorNum)
         return errorNum;
 
-    if (this->size == 0)
+    if (size == 0)
     {
-        LOG(msgNoArgs, ">> Stack is empty", "STK_EMPTY", "");
+        STACK_LOG(msgNoArgs, ">> Stack is empty", "STK_EMPTY", "");
         return STK_EMPTY;
     }
 
-    --this->size;   
+    --size;   
     if (num != NULL)     //Попаем значение, затем заполняем его ядом
-        *num = this->data_[this->size];
-    this->data_[this->size] = poison;
+        *num = *(getDataPtr() + size);
+    *(getDataPtr() + size) = poison;
 
-#ifdef SECURE
-    *(this->hash) = hashFunc();
-#endif
+    HASH_REFRESH();
 
-    LOG(msgNoArgs, ">> Number was popped", "STK_POP", "");
+    STACK_LOG(msgNoArgs, ">> Number was popped", "STK_POP", "");
     return stackVerificator();
 }
 
 template <typename T>
 int stack<T>::stackPrint(int option)
 {
-    assert(this != NULL);
-    FILE *fileName = NULL;
-#ifdef DEBUG
-    fileName = this->logFile;
-    if (option)
-        fileName = stderr;
-#else
-    fileName = stderr;
-#endif
-    
+    FILE *fileName = stderr;
+    DUMP_TO_LOGFILE();
+
     int errorNum = stackVerificator();
     if (errorNum)
         return errorNum;
-        
-    fprintf(fileName, "\n>> Stack info:\n");
-    fprintf(fileName, "\n>> Size: %d", this->size);
-    fprintf(fileName, "\n>> Capacity: %d", this->capacity);
-    fprintf(fileName, "\n>> Current data_ in stack:\n");    
-    for (int i = 0; i < this->size; i++)
+
+    fprintf(fileName, "--------------------------------------------------------------------------------\n");
+    fprintf(fileName, "\n>> Stack dump:\n");
+    fprintf(fileName, "\n>> Size: %d", size);
+    fprintf(fileName, "\n>> Capacity: %d", capacity);
+    fprintf(fileName, "\n>> Current data in stack:\n");    
+    for (int i = 0; i < size; i++)
     {
         fprintf(fileName, "%d) ", i + 1);
-        data_print(fileName, this->data_[i]);        
+        data_print(fileName, *(getDataPtr() + i));        
     }
 
 #ifdef SECURE
-    fprintf(fileName, "Poison is: ");
+    fprintf(fileName, ">> Poison is: ");
     data_print(fileName, poison);
-    CANARYPRINT(*(this->leftCanaryPtr), *(this->rightCanaryPtr));
-    HASHPRINT(*(this->hash));
+    CANARYPRINT(*(getLeftCanaryPtr()), *(getRightCanaryPtr()));
+    fprintf(fileName, ">> Class canaries:\n");
+    CANARYPRINT(classLCanary, classRCanary);
+    HASHPRINT(*(getHashNumberPtr()));
 #endif
+
+    fprintf(fileName, "--------------------------------------------------------------------------------\n");
 
     return 0;
 }
@@ -218,7 +202,6 @@ int stack<T>::stackPrint(int option)
 template <typename T>
 int stack<T>::stk_realloc(const int num)
 {
-    assert(this != NULL);
     assert(num == UP || num == DOWN);
 
     int errorNum = stackVerificator();
@@ -227,53 +210,35 @@ int stack<T>::stk_realloc(const int num)
 
     if (num == UP)
     {
-        this->capacity *= 2;
-        LOG(msgNoArgs, ">> Capacity doubled", "CAPACITY_UP", "");
+        capacity *= 2;
+        STACK_LOG(msgNoArgs, ">> Capacity doubled", "CAPACITY_UP", "");
     }
     else if(num == DOWN)
     {
-        if (this->capacity == 1)
+        if (capacity == 1)
         {
-            LOG(msgNoArgs, ">> Memory reallocatiom error", "MEM_RLC_ERR", "[error]");
+            STACK_LOG(msgNoArgs, ">> Memory reallocatiom error", "MEM_RLC_ERR", "[error]");
             return MEM_RLC_ERR;
         }
         
-        this->capacity /= 2;
-        LOG(msgNoArgs, ">> Capacity halfed", "CAPACITY_DOWN", "");
-    }
-#ifdef SECURE
-    canary *temp = (canary *) \
-        realloc(this->leftCanaryPtr, this->capacity * sizeof(T) + 2 * sizeof(canary) + sizeof(hash_t));
-    if (temp == NULL)
-    {
-        LOG(msgNoArgs, ">> Memory reallocatiom error", "MEM_RLC_ERR", "[error]");
-        return MEM_RLC_ERR;
-    }        
-
-    this->leftCanaryPtr = temp;
-    this->hash = (hash_t *)(temp + 1);
-    this->data_ = (T *)(this->hash + 1);             //Присваиваем указатели данным, хешу и канарейкам
-
-    this->rightCanaryPtr = (canary *)(this->data_ + this->capacity);
-    *(this->rightCanaryPtr) = Rcanary;      //Присваиваем значение новой конечной канарейке, значение старой сотрётся ядом
-#else
-    T *temp = (T *)realloc(this->data_, this->capacity * sizeof(T));
-    if (temp == NULL)
-    {
-        LOG(msgNoArgs, ">> Memory reallocatiom error", "MEM_RLC_ERR", "[error]");
-        return MEM_RLC_ERR;
+        capacity /= 2;
+        STACK_LOG(msgNoArgs, ">> Capacity halfed", "CAPACITY_DOWN", "");
     }
 
-    this->data_ = temp;
-#endif
+    T *memHolder = (T *)realloc(data, STACK_SIZE);
+    if (!memHolder)
+    {
+        STACK_LOG(msgNoArgs, ">> Memory reallocatiom error", "MEM_RLC_ERR", "[error]");
+        return MEM_RLC_ERR;
+    }
+    data = memHolder;
+    *getRightCanaryPtr() = Rcanary;
 
     if (num == UP)
         if (poisonFunc())
         return PSN_ERR;
     
-#ifdef SECURE
-    *(this->hash) = hashFunc();
-#endif
+    HASH_REFRESH();
 
     return stackVerificator();
 }
@@ -282,25 +247,23 @@ template <typename T>
 hash_t stack<T>::hashFunc()
 {
     hash_t hashHolder = 0;
-    for (int i = 0; i < this->size; i++)
-        hashHolder += this->data_[i] * this->size;
-    hashHolder += this->capacity;
-    hashHolder += this->size;
+    for (int i = 0; i < size; i++)
+        hashHolder += *(getDataPtr() + i) * size;
+    hashHolder += capacity;
+    hashHolder += size;
     
     return hashHolder;
 }
 
 template <typename T>
 int stack<T>::poisonFunc()
-{
-    if (this == NULL)
+{   
+    for (int i = size; i < capacity; i++)
     {
-        LOG(msgNoArgs, ">> Poison error: given a NULL pointer", "NULL_PTR", "[error]");
-        return 1;
+        //fprintf(stderr, "dataPtr: %p, data before: %d\n\n", getDataPtr() + i, *(getDataPtr() + i));
+        *(getDataPtr() + i) = poison;
+        //fprintf(stderr, "data after: %d\n\n", *(getDataPtr() + i));
     }
-    
-    for (int i = this->size; i < this->capacity ; i++)
-        this->data_[i] = poison;
 
     return 0;
 }
@@ -308,8 +271,8 @@ int stack<T>::poisonFunc()
 template <typename T>
 int stack<T>::poisonCheck()
 {
-    for (int pos = this->size; pos < this->capacity; pos++)
-        if (this->data_[pos] != poison)
+    for (int pos = size; pos < capacity; pos++)
+        if (*(getDataPtr() + pos) != poison)
             return PSN_ERR;
   
     return 0;
@@ -318,7 +281,7 @@ int stack<T>::poisonCheck()
 template <typename T>
 int stack<T>::canaryCheck()
 {
-    if (*(this->leftCanaryPtr) != Lcanary || *(this->rightCanaryPtr) != Rcanary)
+    if (*(getLeftCanaryPtr()) != Lcanary || *(getRightCanaryPtr()) != Rcanary)
         return CANARY_ERR;
         
     return 0;
@@ -327,7 +290,7 @@ int stack<T>::canaryCheck()
 template <typename T>
 int stack<T>::hashCheck()
 {
-    if (*(this->hash) != hashFunc())
+    if (*(getHashNumberPtr()) != hashFunc())
         return HASH_ERR;
 
     return 0;
@@ -336,32 +299,33 @@ int stack<T>::hashCheck()
 template <typename T>
 int stack<T>::stackVerificator()
 {
-    if (this == NULL)
+#ifdef SECURE
+    if (capacity == 0)
     {
-        LOG(msgNoArgs, ">> Given a NULL pointer", "NULL_ERR", "[error]", "");
-        return NULL_PTR;
-    }
-    if (this->capacity == 0)
-    {
-        LOG(msgNoArgs, ">> Capacity can't be zero", "CAPACITY_ZERO", "[error]", "");
+        STACK_LOG(msgNoArgs, ">> Capacity can't be zero", "CAPACITY_ZERO", "[error]", "");
         return CAPACITY_ZERO;
     }
     if (poisonCheck())
     {
-        LOG(msgNoArgs, ">> Number is not poison", "PSN_ERR", "[error]", "");
+        STACK_LOG(msgNoArgs, ">> Number is not poison", "PSN_ERR", "[error]", "");
         return PSN_ERR;
     }
     if (canaryCheck())
     {
-        LOG(msgNoArgs, ">> Canary is damaged. ", "CANARY_ERR", "[error]", "");
+        STACK_LOG(msgNoArgs, ">> Canary is damaged. ", "CANARY_ERR", "[error]", "");
         return CANARY_ERR;
     }
     if (hashCheck())
     {
-        LOG(msgNoArgs, ">> Hash is not matching.", "HASH_ERR", "[danger]", "");
+        STACK_LOG(msgNoArgs, ">> Hash is not matching.", "HASH_ERR", "[danger]", "");
         return HASH_ERR;
     }
-    
+    if (classLCanary != CLASS_L_CANARY_VAL || classRCanary != CLASS_R_CANARY_VAL)
+    {
+        STACK_LOG(msgNoArgs, ">> Class canary is damaged. ", "CANARY_ERR", "[error]", "");
+        return CANARY_ERR;
+    }
+#endif  
     return 0;
 }
 
@@ -379,6 +343,5 @@ void data_print(FILE *fileName, const long long int num)
 {
     fprintf(fileName, "%lld\n", num);
 }
-
 
 #endif
