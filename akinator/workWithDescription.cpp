86aea55d51a40860743f67eecf1673fd88c442df
yayamayaya <semenov.aleksandr@phystech.edu>
2024-03-19 01:38:24 +0300

    добавлены функции описания предмета

--- akinator/workWithDescription.cpp
+++ akinator/workWithDescription.cpp
@@ -3,68 +3,128 @@
 #include <string.h>
 #include "akinator.h"
 
-int takeDescription(class stack<char> *description, node_t *node, char *objName)
+int findDescription(class Stack<char> *description, node_t *node, char *objName)
 {
     assert(node);
     assert(objName);
 
     if (!node->left && !node->right)
-        if (!strcmp(node->data, objName))
-        {
-             //printf(">>> object found\n");
-            return 1;            
-        }
-        else
-            return 0;
-    
+        return !strcmp(node->data, objName);
+        
     node_t *foundedNode = NULL;
         
     if (node->left)
     {
-        //printf(">> going to the left node\n\n");
+        //printf(">>  >> going to the left node\n\n");
         description->stackPush(1);
-        if (takeDescription(description, node->left, objName))
+        if (findDescription(description, node->left, objName))
             return 1; 
-        //printf(">> node in the left not found, going next\n\n");
+        //printf(">>  >> node in the left not found, going next\n\n");
         description->stackPop(NULL);    
     }
     if (node->right)
     {
-        //printf(">> going to the right node\n\n");
+        //printf(">>  >> going to the right node\n\n");
         description->stackPush(0);
-        if (takeDescription(description, node->right, objName))
+        if (findDescription(description, node->right, objName))
             return 1;  
-        //printf(">> node in the right not found, going next\n\n");
+        //printf(">>  >> node in the right not found, going next\n\n");
         description->stackPop(NULL);            
     }
 
     return 0;
 }
 
-class stack<char> giveDescription(node_t *root, const char *descrLogName)
+class Stack<char> takeDescription(node_t *root, const char *descrLogName)
 {
     assert(root);
 
     char *object = NULL;
     takeAString(&object);
 
-    stack<char> description = {};
+    Stack<char> description = {};
     description.stackCtor(10, descrLogName);
-    takeDescription(&description, root, object);
-    description.stackPrint(IN_CONSOLE);
+    findDescription(&description, root, object);
+    //description.stackPrint(IN_CONSOLE);
     free(object);
+
+    if (description.size == 0)
+        printf(">>  Given object not found.\n");
+    
 
     return description;
 }
 
-int compareTwoObjects(node_t *root)
+void giveDescription(node_t *node)
 {
-    assert(root);
+    printf(">>  Give a name of an object, which description you want to find:\n");
+    Stack<char> description = takeDescription(node, "descriptionLog.log");
 
-    printf("Give a name of an object, which you want to compare:\n");
-    stack<char> description1 = giveDescription(root, "descriptionLog1.log");
-    printf("Give a name of another object:\n");
-    stack<char> description2 = giveDescription(root, "descriptionLog2.log");
+    if (description.size == 0)
+    {
+        description.stackDtor();
+        return;
+    }
+    
+    printf(">>  Given object has next description:\n");
+    for (int i = 0; i < description.size; i++)
+    {
+        if(node == NULL)
+            break;
+
+        if (description.getDataOnPos(i))
+        {
+            printf("%s", node->data);
+            node = node->left;
+        }
+        else
+        {   
+            printf("not %s", node->data);
+            node = node->right;
+        }
+
+        if (i == description.size - 1)
+            printf(".\n", node->data);     
+        else 
+            printf(", ");       
+
+    }   
+    
+    description.stackDtor();
+    return;
+}
+
+int compareTwoObjects(node_t *node)
+{
+    assert(node);
+
+    printf(">>  Give a name of an object, which you want to compare:\n");
+    Stack<char> description1 = takeDescription(node, "descriptionLog1.log");
+    printf(">>  Give a name of another object:\n");
+    Stack<char> description2 = takeDescription(node, "descriptionLog2.log");
+
+    printf(">> These objects are:\n");
+
+    for (int i = 0; i < description1.size && i < description2.size; i++)
+    {
+        if (description1.getDataOnPos(i) == description2.getDataOnPos(i))
+            if (description1.getDataOnPos(i))
+            {
+                printf("both %s", node->data);
+                node = node->left;
+                continue;
+            }
+            else
+            {   
+                printf("both not %s", node->data);
+                node = node->right;
+                continue;
+            }
+        
+    }
+    
+    
+    
     description1.stackDtor();
     description2.stackDtor();
 
